868ab0db0557ae10f7b8b862887587f46c792043
Syed Ali Ashraf <syeddaliashraf@gmail.com>
2026-07-28 20:41:42 +0530

    Lesson 19 - Greater Than or Equal Operator ()

---
+++ 02_Queries/19_gte_operator.py
@@ -0,0 +1,61 @@
+"""
+File:
+    19_gte_operator.py
+
+Purpose:
+    Demonstrate MongoDB Greater Than or Equal Operator ($gte).
+
+Author:
+    Syed Ali Ashraf
+
+Course:
+    MongoDB Bootcamp 2.0
+"""
+
+from pymongo import MongoClient
+
+from Dataset.config import (
+    MONGO_URI,
+    DATABASE_NAME
+)
+
+
+def get_customers():
+
+    client = MongoClient(MONGO_URI)
+
+    database = client[DATABASE_NAME]
+
+    customers = database["customers"]
+
+    cursor = customers.find(
+        {
+            "age": {
+                "$gte": 30
+            }
+        },
+        {
+            "_id": 0,
+            "customerID": 1,
+            "name": 1,
+            "age": 1
+        }
+    )
+
+    return cursor
+
+
+def main():
+
+    customers = get_customers()
+
+    print("=" * 60)
+    print("CUSTOMERS AGE >= 30")
+    print("=" * 60)
+
+    for customer in customers:
+        print(customer)
+
+
+if __name__ == "__main__":
+    main()
